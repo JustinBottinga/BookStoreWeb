8d880a3178636c525e0c46bafe9ca99921beae75
Justin Bottinga <0337445@student.rocvantwente.nl>
2024-11-29 11:02:24 +0100

    homepage klaar

--- BookStore.Models/Category.cs
+++ BookStore.Models/Category.cs
@@ -10,9 +10,12 @@ namespace BookStore.Models
         [MaxLength(50, ErrorMessage = "{0} mag maximaal {1} tekens bevatten")]
         [Display(Name = "Naam")]
         public string Name { get; set; }
-        [Display(Name = "Volgnummer")]
+
         [Range(minimum:1,maximum:100, ErrorMessage = "{0} moet tussen {1} en {2} liggen.")]
+        [Required(ErrorMessage = "{0} is een verplicht veld")]
+        [Display(Name = "Volgnummer")]
         public int DisplayOrder { get; set; }
+
         [Display(Name="Aanmaakdatum")]
         public DateTime CreatedDateTime { get; set; } = DateTime.Now;
 
